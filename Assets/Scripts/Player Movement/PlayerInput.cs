6a0fe379cb508c6f9115d7e6ab9ef887c38b9033
Chathander <evan.alexander@chatham.edu>
2023-11-01 11:58:34 -0400

    Evans got a fork - updated tags / folders
    Made folders that I will manage, added tags for possible collision solutions

---
+++ Assets/Scripts/Player Movement/PlayerInput.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    /// <summary>
+    /// input Recieve from the tilt 5 controller.
+    /// Using the rotation of the joystick as a basline control for the positon
+    /// of the paddle.
+    /// </summary>
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
