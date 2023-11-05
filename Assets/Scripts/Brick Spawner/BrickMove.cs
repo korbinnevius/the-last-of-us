78338147a020f67863e9babc504396a3732ce45a
samcaloiero <122567577+samcaloiero@users.noreply.github.com>
2023-11-05 13:30:59 -0500

    GameManager

--- Assets/Scripts/Brick Spawner/BrickMove.cs
+++ Assets/Scripts/Brick Spawner/BrickMove.cs
@@ -6,20 +6,21 @@ public class BrickMove : MonoBehaviour
 {
     //Drag Player Transform Here
     public Transform target;
-    public int speed = 3;
+    public float speed = 3;
+
     void Start()
     {
-       MoveBrick();
+        MoveBrick();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void MoveBrick()
     {
-        transform.position = Vector3.MoveTowards(transform.position,target.position, speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
     }
 }
--- Assets/Scripts/Brick Spawner/OtherBrickSpawner.cs
+++ Assets/Scripts/Brick Spawner/OtherBrickSpawner.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,10 @@ public class CenterSurrounder : MonoBehaviour
     public GameObject OriginalSurrounderObject;
     public int SurrounderObjectCount;
     public BrickMove _brickMove;
+    
+    //Movement Variables
+    public Transform target;
+    public float speed = 3.0f;
 
     private readonly float AppearWaitDuration = 0.3f;
     private Transform SurrounderParentTransform;
@@ -16,9 +21,14 @@ public class CenterSurrounder : MonoBehaviour
     {
         SurrounderParentTransform = new GameObject(gameObject.name + " Surrounder Parent").transform;
         StartCoroutine(SurroundStepAnimated());
-        _brickMove.MoveBrick();
+        transform.position = Vector3.MoveTowards(transform.position,target.position, speed * Time.deltaTime);
 
         //Surround();
+    }
+
+    private void Update()
+    {
+        _brickMove.MoveBrick();
     }
 
     IEnumerator SurroundStepAnimated()
@@ -39,5 +49,9 @@ public class CenterSurrounder : MonoBehaviour
             yield return new WaitForSeconds(AppearWaitDuration);
         }
 
+    }
+    public void MoveBrick()
+    {
+        transform.position = Vector3.MoveTowards(transform.position,target.position, speed * Time.deltaTime);
     }
 }
