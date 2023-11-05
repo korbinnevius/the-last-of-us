86aaca9081c9267968d955314dc6c62ebf3341ac
Chathander <evan.alexander@chatham.edu>
2023-11-05 18:13:12 -0500

    Got a simple tilt 5 input script up
    It is now time for some input refining. Both the keyboard and the tilt 5 inputs are very basic, I would like to add some feel to them or add values to increase their usfullness.
    
    some thoughts are having the axis of the tilt 5 stick bounce the quaternion over to where you point the stick. Maybe not imediatly, but hopefully snappy witha  bit of smoothing so the paddle is not too gittery. TIME FOR MORE RESEARCH AND MATHHHHH.

---
+++ Assets/Scripts/Evan/Player Movement/PreliminaryT5Input.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using TiltFive;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Input = TiltFive.Input;
+
+public class PreliminaryT5Input : MonoBehaviour
+{
+
+    private ControllerIndex _controllerIndex;
+    private PlayerIndex _playerIndex;
+    private PaddleController _paddleController;
+
+    [FormerlySerializedAs("leftOrRightFloat")] public float StickAxisValueFloat;
+    void Start()
+    {
+        _paddleController = GetComponent<PaddleController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        stickinput();
+    }
+    
+    // set a git stick tilt ot a float value, if past a certain value set left or right to work inside this script
+    // from the player input
+    private void stickinput()
+    {
+        StickAxisValueFloat = Input.GetStickTilt().x;
+
+        if (StickAxisValueFloat >= 0.8f)
+        {
+            _paddleController.RotateRight();
+        }
+        else if (StickAxisValueFloat <= -0.8f )
+        {
+            _paddleController.RotateLeft();
+        }
+        else
+        {
+            _paddleController.NoRotation();
+        }
+    }
+   
+}
