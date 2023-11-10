c15bf3e74bbbb8195a91cdf1f5b3dd8185c55213
samcaloiero <122567577+samcaloiero@users.noreply.github.com>
2023-11-10 15:29:23 -0500

    game manager shit

--- Assets/Scripts/BrickBehavior/Brick Spawner/OtherBrickSpawner.cs
+++ Assets/Scripts/BrickBehavior/Brick Spawner/OtherBrickSpawner.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 
 public class CenterSurrounder : MonoBehaviour
@@ -10,34 +11,50 @@ public class CenterSurrounder : MonoBehaviour
     public float Radius;
     public int SurrounderObjectCount;
     private float angleStep;
-
+    public GameObject brick;
+    public GameMGRScriptableObject GameMgrScriptableObject;
+    
+    
+    private int spawnProbability;
+    private GameObject empty;
     private readonly float AppearWaitDuration = 0.3f;
     private Transform SurrounderParentTransform;
+  
 
     void Start()
     {
-        //SurrounderParentTransform = new GameObject(gameObject.name + " Surrounder Parent").transform;
+        spawnProbability = GameMgrScriptableObject.spawnProbability;
+        empty = new GameObject();
         SurrounderParentTransform = transform;
-        // StartCoroutine(SurroundStepAnimated());
         SurroundStepAnimated();
+        
     }
-    
-    // IEnumerator SurroundStepAnimated()
+
+    private void Update()
+    {
+       
+    }
+
+    // void SurroundStepAnimated()
     // {
-    //     yield return new WaitForSeconds(AppearWaitDuration);
-    //
-    //          angleStep  /= SurrounderObjectCount;
-    //
-    //     OriginalSurrounderObject.transform.SetParent(SurrounderParentTransform);
-    //
-    //     for (int i = 1; i < SurrounderObjectCount; i++)
+    //     for (int i = 0; i < SurrounderObjectCount; i++)
     //     {
-    //         GameObject newSurrounderObject = Instantiate(OriginalSurrounderObject);
+    //         float radius = Radius;
+    //         //Getting percents from Amnt of Objects
+    //         float percent = i / (float)SurrounderObjectCount;
+    //         //remap to range 0->2pi.
+    //         float x = percent * 2*Mathf.PI;
+    //         //dir is position on unit circle
+    //         Vector3 dir = new Vector3(Mathf.Cos(x), 0, Mathf.Sin(x)).normalized;
+    //         //offseting 
+    //         Vector3 pos = transform.position + dir*radius;
+    //         //
+    //         Quaternion orientation = Quaternion.LookRotation(-dir,Vector3.up);
+    //         GameObject newSurrounderObject = Instantiate(OriginalSurrounderObject,pos,orientation,SurrounderParentTransform);
+    //         
+    //        // newSurrounderObject.transform.RotateAround(wrapDistance, Vector3.up, angleStep * i);
     //
-    //         newSurrounderObject.transform.RotateAround(transform.position, Vector3.up, angleStep * i);
-    //         newSurrounderObject.transform.SetParent(SurrounderParentTransform);
-    //
-    //         yield return new WaitForSeconds(AppearWaitDuration);
+    //         
     //     }
     //
     // }
@@ -45,6 +62,7 @@ public class CenterSurrounder : MonoBehaviour
     {
         for (int i = 0; i < SurrounderObjectCount; i++)
         {
+            int spawnPick = Random.Range(0, spawnProbability);
             float radius = Radius;
             //Getting percents from Amnt of Objects
             float percent = i / (float)SurrounderObjectCount;
@@ -56,10 +74,18 @@ public class CenterSurrounder : MonoBehaviour
             Vector3 pos = transform.position + dir*radius;
             //
             Quaternion orientation = Quaternion.LookRotation(-dir,Vector3.up);
-            GameObject newSurrounderObject = Instantiate(OriginalSurrounderObject,pos,orientation,SurrounderParentTransform);
+            if (spawnPick == 0)
+            {
+                Instantiate(brick,pos,orientation,SurrounderParentTransform);
+            }
+            else
+            {
+                Instantiate(empty,pos,orientation,SurrounderParentTransform);
+            }
             
-           // newSurrounderObject.transform.RotateAround(wrapDistance, Vector3.up, angleStep * i);
-
+            // newSurrounderObject.transform.RotateAround(wrapDistance, Vector3.up, angleStep * i);
+            
+            
             
         }
 
