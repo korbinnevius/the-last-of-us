be558d7c63fa71e938ee0b24cf6ce92090e28142
Chathander <evan.alexander@chatham.edu>
2023-11-13 19:35:48 -0500

    General adjustments, readme

---
+++ Assets/ReadMe.txt
@@ -0,0 +1,30 @@
+Evan Alexander, Korbin Nevius, Malik George, Ethan Dersteine, Sam Caloiero
+IMM 406 Prototyping For Advanced Systems
+Fall 23
+
+Prototype 2: The Rest Of Us
+
+
+Requirements:
+Unity 2022.3.12f1
+Tilt 5 Headset and XE Board - virtically mounted.
+Tilt 5 Control Pannel
+Ultra Leap 2 Hand Tracker.
+Ultra Leap Control pannel.
+A dark or dimly lit room.
+
+Set Up:
+- Mount The Tilt 5 XE Board with the center just under head height hanging on a wall or by clamps on a mettal rod.
+- Mount the UltraLeap Hand tracker just below the board anlged 30 - 45 degress up towards the player.
+- For functionality of the tilt 5 glasses with a mounted board, adjust the default orientation of the headset 
+detection angle by 90 on the pitch and hit apply.
+(aplication found: C:\Program Files\TiltFive\SDK\Utils\gameboard_transform.exe)
+
+Controlls:
+- The location of your hand around the mounted hand tracker will orient the position of the paddle around the playing area.
+Hand above the tracker will move the paddle up, right toward the right, etc.
+- Squeeze hand into a fist to launch the ball forward.
+
+
+Sources:
+
--- Assets/Scripts/Malik/CameraShake.cs
+++ Assets/Scripts/Malik/CameraShake.cs
@@ -8,31 +8,33 @@ public class CameraShake : MonoBehaviour
 
     public AnimationCurve curve;
 
-    public bool start = false;
+    public GameObject Tilt5Board;
+
+    //public bool start = false;
     
    
     
     // Update is called once per frame
-    void Update()
-    {
-        if (start)
-        {
-            start = false;
-            StartCoroutine(Shaking());
-        }
-    }
-
-    // private void OnCollisionEnter(Collision collision)
+    // void Update()
     // {
-    //     if (collision.collider.CompareTag("Brick"))
+    //     if (start)
     //     {
+    //         start = false;
     //         StartCoroutine(Shaking());
     //     }
     // }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Brick"))
+        {
+            StartCoroutine(Shaking());
+        }
+    }
+
     IEnumerator Shaking()
     {
-        Vector3 startPosition = transform.position; //stores the starting position of the camera.
+        Vector3 startPosition = Tilt5Board.transform.position; //stores the starting position of the camera.
         float elaspedTime = 0f; //stores the elapsed time since the beginning of the shake.
         
         //while the elapsed time is less that the duration, move the camera position in a random vector.
@@ -40,10 +42,10 @@ public class CameraShake : MonoBehaviour
         {
             elaspedTime += Time.deltaTime;
             float strength = curve.Evaluate(elaspedTime / duration);
-            transform.position = startPosition + Random.insideUnitSphere * strength;
+            Tilt5Board.transform.position = startPosition + Random.insideUnitSphere * strength;
             yield return null; //to continue the execution at the next frame.
         }
 
-        transform.position = startPosition; // returning the camera back to its original position.
+        Tilt5Board.transform.position = startPosition; // returning the camera back to its original position.
     }
 }
